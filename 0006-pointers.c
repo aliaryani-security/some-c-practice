8b2cb722ba157fb7ae50d665167fbad40ff9c03c
Ali Aryani <lenesis@proton.me>
2025-10-03 01:16:38 +0330

    simple pointer to string in c

---
+++ 0006-pointers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str_a[20];
+    char *pointer;
+    char *pointer2;
+
+    strcpy(str_a, "Hello World!\n");
+    pointer = str_a;
+    puts(str_a);
+
+    pointer2 = pointer + 2;
+    puts(pointer2);
+    strcpy(pointer2,"y you guys!\n");
+    puts(pointer);
+    return 0;
+}
