1cedfbe7d8329cfe0038cdd9c92dcdca39929eff
Ali Aryani <lenesis@proton.me>
2025-10-03 11:38:59 +0330

    convert to assembly

---
+++ 0009-commandline.s
@@ -0,0 +1,75 @@
+	.file	"0009-commandline.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"There were %d arguments provided.\n"
+.LC1:
+	.string	"argument #%d\t-\t%s\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 32
+	mov	DWORD PTR -20[rbp], edi
+	mov	QWORD PTR -32[rbp], rsi
+	mov	eax, DWORD PTR -20[rbp]
+	mov	esi, eax
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	mov	DWORD PTR -4[rbp], 0
+	jmp	.L2
+.L3:
+	mov	eax, DWORD PTR -4[rbp]
+	cdqe
+	lea	rdx, 0[0+rax*8]
+	mov	rax, QWORD PTR -32[rbp]
+	add	rax, rdx
+	mov	rdx, QWORD PTR [rax]
+	mov	eax, DWORD PTR -4[rbp]
+	mov	esi, eax
+	lea	rax, .LC1[rip]
+	mov	rdi, rax
+	mov	eax, 0
+	call	printf@PLT
+	add	DWORD PTR -4[rbp], 1
+.L2:
+	mov	eax, DWORD PTR -4[rbp]
+	cmp	eax, DWORD PTR -20[rbp]
+	jl	.L3
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Gentoo 14.3.1_p20250801 p4) 14.3.1 20250801"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
