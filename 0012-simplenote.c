589cb751842ab273656cf000c30f9d1144b8dd96
Ali Aryani <lenesis@proton.me>
2025-10-03 18:30:31 +0330

    wrapper declarations; buffer and datafile declaration;

--- 0012-simplenote.c
+++ 0012-simplenote.c
@@ -8,3 +8,27 @@ void usage (char *program_name, char *file_name) {
     printf("Usage: %s <data to add to %s>\n",program_name, file_name);
     exit(0);
 }
+
+void fatal(char *);
+void ec_malloc (unsigned int); // an error-checked malloc() wrapper
+
+int main (int argc, char *argv[]) {
+    int fd; //file descriptor
+    char *buffer, *datafile;
+
+    buffer = (char *) ec_malloc(100);
+    datafile = (char *) ec_malloc(20);
+    strcpy(datafile, "/tmp/notes");
+
+    if(argc < 2)
+        usage(argv[0], datafile);
+
+    strcpy(buffer, argv[1]);
+
+    printf("[DEBUG] buffer @ %p \'%s\'\n",buffer, buffer);
+    printf("[DEBUG] datafile @ %p \'%s\'\n",datafile, datafile);
+
+    strncat (buffer, "\n", 1); // add a newline to the end
+
+    return 0;
+}
